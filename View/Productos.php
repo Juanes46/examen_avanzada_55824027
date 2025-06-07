ebb4bf79552c9a94f8bb76c8d8314c0d07ecc32d
Sala E19 <CSalaE19@uniboyaca.edu.co>
2025-06-07 12:31:40 -0500

    Hola :)

--- View/Productos.php
+++ View/Productos.php
@@ -2,33 +2,32 @@
 include '../../models/drivers/conexDB.php';
 include '../../models/entities/entity.php';
 include '../../models/entities/category.php';
-include '../../controllers/ControllerInventario.php';
+include '../../controllers/categoriesController.php';
 
-use app\controllers\ControllerInventario;
+use app\controllers\CategoriesController;
 
-$controller = new ControllerInventario();
+$controller = new CategoriesController();
 $categories = $controller->queryAllCategories();
 ?>
-
-
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inventario</title>
+    <title>Categorías</title>
     <link rel="stylesheet" href="../../style.css">
 </head>
 <body>
     <div class="container">
-
-        <h1>Inventario</h1>
-<form action="welcome.php" method="POST">
-Nombre: <input type="text" name="Nombre"><br>
-Cantidad: <input type="text" name="Cantidad"><br>
-Precio Unitario: <input type="number" name="Precio"><br>
-<input type="submit">
-</form>
+            <h1>Categorías</h1>
+        <div class="menu">
+            <input type="text" >
+        </div>
+        <?php if (isset($_GET['result'])): ?>
+            <p>
+                <?= ($_GET['result'] === 'success') ? 'Operación realizada con éxito.' : 'Error al realizar la operación.' ?>
+            </p>
+        <?php endif; ?>
 
       
 </body>
